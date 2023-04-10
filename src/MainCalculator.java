b24fbd0326719e4b77c40242da46fb9e1e53dab0
Dmytro Abalmas <abalmasjava@gmail.com>
2023-04-10 19:39:57 +0300

    Commit1

--- src/MainCalculator.java
+++ src/MainCalculator.java
@@ -1,4 +1,4 @@
-public class Main {
+public class MainCalculator {
     public static void main(String[] args) {
 
 
--- src/Operation.java
+++ src/Operation.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Operation {
+public class Operation {
+
+    public static int Adding(int operand1, int operand2) {
+        return operand1 + operand2;
+    }
+
+    public static int Subtraction(int operand1, int operand2) {
+        return operand1 - operand2;
+    }
+
+    public static int Multiplication(int operand1, int operand2) {
+        return operand1 * operand2;
+    }
+
+    public static int Division(int operand1, int operand2) {
+        return operand1 / operand2;
+    }
+
+
 }
+
+
